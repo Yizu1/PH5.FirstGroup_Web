08384e1f4304b75c4354d858b2b1edee16804161
yuand123 <2155696909@qq.com>
2021-05-22 09:05:33 +0800

    1

---
+++ PH5.FirstGroup_Web/Pages/store_management/Inventory/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PH5.FirstGroup_Web.Pages.store_management.Inventory
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
